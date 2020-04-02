a5532c83775e770e77877d7dd2e2de95f5808be4
Rolfe Schmidt <rolfeschmidt@gmail.com>
2020-04-02 16:37:29 -0500

    unit tests for hash module

---
+++ src/__tests__/hash.test.ts
@@ -0,0 +1,75 @@
+import { Hash } from '../hash'
+import { readFileDataInDictionary, readHexString } from '../testutilities/utilities'
+
+import cryptoMath from '../msrcrypto/cryptoMath'
+import L2048N256 from '../SubgroupL2048N256'
+import ECP256 from '../EcP256'
+import cryptoECC from '../msrcrypto/cryptoECC'
+
+const vectors = readFileDataInDictionary('testvectors_hashing.txt')
+
+const bytesx0102030405 = readHexString('0102030405')
+
+test('hash_byte (0x01)', () => {
+    const testCase = 'hash_byte (0x01)'
+    const H = new Hash()
+    H.updateByte(0x01)
+    const expectedValue = '4bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c385a5d7cce23c7785459a'
+    const digest = H.digest()
+    const hexDigest = Buffer.from(digest).toString('hex')
+    expect(hexDigest).toBe(expectedValue)
+    expect(cryptoMath.sequenceEqual(digest, readHexString(vectors[testCase]))).toBeTruthy()
+})
+
+test('hash_octetstring (0x0102030405', () => {
+    const testCase = 'hash_octectstring (0x0102030405)'
+    const H = new Hash()
+    H.updateBytes(bytesx0102030405)
+    expect(cryptoMath.sequenceEqual(H.digest(), readHexString(vectors[testCase]))).toBeTruthy()
+})
+
+test('hash_null (null)', () => {
+    const testCase = 'hash_null (null)'
+    const H = new Hash()
+    H.updateNull()
+    expect(cryptoMath.sequenceEqual(H.digest(), readHexString(vectors[testCase]))).toBeTruthy()
+})
+
+test('"hash_list [0x01, 0x0102030405, null]', () => {
+    const testCase = 'hash_list [0x01, 0x0102030405, null]'
+    const H = new Hash()
+    H.updateUint32(3)
+    H.updateByte(0x01)
+    H.updateBytes(bytesx0102030405)
+    H.updateNull()
+    expect(cryptoMath.sequenceEqual(H.digest(), readHexString(vectors[testCase]))).toBeTruthy()
+})
+
+// TODO: get curve definitions
+
+test('hash_group (1.3.6.1.4.1.311.75.1.1.1)', () => {
+    const testCase = 'hash_group (1.3.6.1.4.1.311.75.1.1.1)'
+    const H = new Hash()
+    const Gq = L2048N256
+    Gq.updateHash(H)
+    expect(cryptoMath.sequenceEqual(H.digest(), readHexString(vectors[testCase]))).toBeTruthy()
+})
+
+test('hash_group (1.3.6.1.4.1.311.75.1.2.1)', () => {
+    const testCase = 'hash_group (1.3.6.1.4.1.311.75.1.2.1)'
+    const H = new Hash()
+    const Gq = ECP256
+    Gq.updateHash(H)
+
+    expect(cryptoMath.sequenceEqual(H.digest(), readHexString(vectors[testCase]))).toBeTruthy()
+})
+
+test('hash_group wrong_group (1.3.6.1.4.1.311.75.1.2.1)', () => {
+    const testCase = 'hash_group (1.3.6.1.4.1.311.75.1.2.1)'
+    const H = new Hash()
+    const Gq = ECP256
+    Gq.p256 = cryptoECC.createP384()
+    Gq.updateHash(H)
+
+    expect(cryptoMath.sequenceEqual(H.digest(), readHexString(vectors[testCase]))).toBeFalsy()
+})
---
+++ src/testutilities/utilities.ts
@@ -0,0 +1,76 @@
+import fs from 'fs'
+import path from 'path'
+
+export const performanceTimer = performance || Date // performance not supported on Safari
+export const testVectorDirectory = 'src/__tests__/TestVectors'
+
+export function readNumberList(s: string): number[] {
+    const elements = s.split(',')
+    const array = new Array()
+
+    for (let i = 0; i < elements.length; i++) {
+        const n = elements[i].valueOf()
+        array[i] = n
+    }
+
+    return array
+}
+export function readHexString(hexString: string): Uint8Array {
+    const array = new Array()
+    let index = 0
+    if (hexString.length % 2 !== 0) {
+        // prepend 0
+        hexString = '0' + hexString
+    }
+
+    for (let i = 0; i < hexString.length; i += 2) {
+        array[index++] = parseInt('0x' + hexString.substr(i, 2), 16)
+    }
+
+    const result = new Uint8Array(array)
+    return result
+}
+
+function readFileData(filename: string): string {
+    return fs.readFileSync(path.resolve(testVectorDirectory, filename), 'utf8')
+}
+
+export function readFileDataInDictionary(filename: string): { [k: string]: any } {
+    const fileData = readFileData(filename)
+
+    const lines = fileData.split('\r\n')
+    const dictionary = {}
+    for (let j = 1; j < lines.length; j++) {
+        // skip the file header in line 0
+        const lineData = lines[j].split(' = ')
+        dictionary[lineData[0]] = lineData[1]
+    }
+    return dictionary
+}
+
+export function readTestVectors(filename: string): { [k: string]: any } {
+    const vectorsData = readFileData(filename)
+    // put each test vector variable in a dictionary
+    const lines = vectorsData.split('\r\n')
+    const vectors = {}
+    for (let j = 1; j < lines.length; j++) {
+        // skip the header
+        const lineData = lines[j].split(' = ')
+        vectors[lineData[0]] = lineData[1]
+    }
+    return vectors
+}
+
+export function readRecommendedParams(filename: string): { [k: string]: any } {
+    const paramsData = readFileData(filename)
+
+    // put each test vector variable in a dictionary
+    const lines = paramsData.split('\r\n')
+    const vectors = {}
+    for (let j = 1; j < lines.length; j++) {
+        // skip the header
+        const lineData = lines[j].split(' = ')
+        vectors[lineData[0]] = lineData[1]
+    }
+    return vectors
+}
